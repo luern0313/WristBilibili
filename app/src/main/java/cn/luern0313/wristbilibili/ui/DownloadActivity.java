eb464b516fd043d2fe81372aaba4d48478bbfeec
luern0313 <liupeiran0313@163.com>
2018-10-24 13:29:24 +0800

    点标题进入菜单

--- app/src/main/java/cn/luern0313/wristbilibili/ui/DownloadActivity.java
+++ app/src/main/java/cn/luern0313/wristbilibili/ui/DownloadActivity.java
@@ -1,16 +1,27 @@
 package cn.luern0313.wristbilibili.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.luern0313.wristbilibili.R;
 
 public class DownloadActivity extends Activity
 {
+    Context ctx;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+        ctx = this;
+    }
+
+    public void buttonTitle(View view)
+    {
+        Intent intent = new Intent(ctx, MenuActivity.class);
+        startActivity(intent);
     }
 }
