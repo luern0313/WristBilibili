be0ac33746af7ab0e7bc4cf25bc7b8d35cc2ab90
luern0313 <liupeiran0313@163.com>
2018-11-13 14:57:34 +0800

    优化布局

--- app/src/main/java/cn/luern0313/wristbilibili/ui/FollowmeActivity.java
+++ app/src/main/java/cn/luern0313/wristbilibili/ui/FollowmeActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
+import android.widget.LinearLayout;
 
 import cn.luern0313.wristbilibili.R;
 
@@ -14,11 +16,21 @@ import cn.luern0313.wristbilibili.R;
 public class FollowmeActivity extends Activity
 {
     Context ctx;
+
+    CardView cardView;
+    LinearLayout likeView;
+    LinearLayout coinView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_followme);
         ctx = this;
+
+        cardView = findViewById(R.id.fme_card);
+        likeView = findViewById(R.id.fme_like);
+        coinView = findViewById(R.id.fme_coin);
+
+
     }
 }
